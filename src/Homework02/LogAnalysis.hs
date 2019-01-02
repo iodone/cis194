2374726929718ed71a27a15f430d8680336d564c
Your Name <you@example.com>
2019-01-02 13:12:56 +0800

    complete homwwork 2

--- src/Homework02/LogAnalysis.hs
+++ src/Homework02/LogAnalysis.hs
@@ -2,6 +2,7 @@ module Homework02.LogAnalysis where
 
 import Homework02.Log
 
+-- Ex. 1
 -- log string to LogMessage AST
 parseMessage :: String -> LogMessage
 parseMessage message = 
@@ -28,7 +29,48 @@ parse :: String -> [LogMessage]
 parse = (map parseMessage) . lines
         
 
+-- Ex. 2
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t = t
+insert x Leaf = Node Leaf x Leaf
+insert x (Node left rv right) = 
+    let xTimestamp = getLogMessageTimestamp x
+        rvTimestamp = getLogMessageTimestamp rv
+    in 
+        if xTimestamp < rvTimestamp 
+        then Node (insert x left) rv right  
+        else Node left rv (insert x right)
 
 
+getLogMessageTimestamp :: LogMessage -> TimeStamp 
+getLogMessageTimestamp (LogMessage _ time _) = time
+getLogMessageTimestamp _ = 0
+
+--Ex. 3
+build :: [LogMessage] -> MessageTree
+build xs = foldl (\tree x -> insert x tree) Leaf xs
+
+--Ex. 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left rv right) =
+    (inOrder left) ++ [rv] ++ (inOrder right)
+
+-- Ex. 5
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logMsgs = 
+    let sortedMsgs = inOrder . build $ logMsgs
+    in
+        foldr (\x wrongMsgs-> if isRelevantMessage x then (getLogMessageString x):wrongMsgs else wrongMsgs) [] sortedMsgs
 
 
+getLogMessageString :: LogMessage -> String
+getLogMessageString (LogMessage _ _ msg) = msg
+getLogMessageString _ = ""
+
+isRelevantMessage :: LogMessage -> Bool
+isRelevantMessage (LogMessage mType _ _) = 
+    case mType of 
+        (Error level) -> if level >= 50 then True else False
+        _ -> False
+isRelevantMessage _ = False
--- test/Homework02/LogAnalysisSpec.hs
+++ test/Homework02/LogAnalysisSpec.hs
@@ -5,19 +5,83 @@ import Homework02.Log
 
 import Test.Hspec
 
+smallTree :: MessageTree
+smallTree = Node
+    (Node Leaf (LogMessage Info 1 "1111") Leaf)
+    (LogMessage Info 3 "3333")
+    (Node Leaf (LogMessage Info 5 "5555") Leaf)
+
+newTree :: MessageTree
+newTree = Node
+    (Node Leaf (LogMessage Info 1 "1111") (Node Leaf (LogMessage (Error 100) 2 "2222") Leaf))
+    (LogMessage Info 3 "3333")
+    (Node (Node Leaf (LogMessage Info 4 "4444") Leaf) (LogMessage Info 5 "5555") Leaf)
+
+logMessages :: [LogMessage]
+logMessages = 
+    [
+    (LogMessage Info 3 "3333"),
+    (LogMessage Info 1 "1111"),
+    (LogMessage Info 5 "5555"),
+    (LogMessage (Error 100) 2 "2222"),
+    (LogMessage Info 4 "4444")
+    ]
+
+sortedLogMessages :: [LogMessage]
+sortedLogMessages = 
+    [
+    (LogMessage Info 1 "1111"),
+    (LogMessage (Error 100) 2 "2222"),
+    (LogMessage Info 3 "3333"),
+    (LogMessage Info 4 "4444"),
+    (LogMessage Info 5 "5555")
+    ]
+
+postmortemMessages :: [LogMessage]
+postmortemMessages =
+    [ (LogMessage Info 1 "I1")
+    , (LogMessage (Error 40) 2 "E2")
+    , (LogMessage (Error 50) 4 "E4")
+    , (LogMessage (Error 51) 3 "E3")
+    ]
+
+postmortemResult :: [String]
+postmortemResult = ["E3", "E4"]
+
 spec :: Spec
 spec = describe "Homework02.LogAnalysis" $ do
 
-  describe "parseMessage" $ do
-    it "Info log" $ do
-        parseMessage "I 6 Completed armadillo processin" `shouldBe` LogMessage Info 6 "Completed armadillo processin"
+    describe "parseMessage" $ do
+        it "Info log" $ do
+            parseMessage "I 6 Completed armadillo processin" `shouldBe` LogMessage Info 6 "Completed armadillo processin"
 
-    it "Warning log" $ do
-        parseMessage "W 5 Flange is due for a check-up" `shouldBe` LogMessage Warning 5 "Flange is due for a check-up"
-        
-    it "Error log" $ do
-        parseMessage "E 20 2 Too many pickles" `shouldBe` LogMessage (Error 20) 2 "Too many pickles"
+        it "Warning log" $ do
+            parseMessage "W 5 Flange is due for a check-up" `shouldBe` LogMessage Warning 5 "Flange is due for a check-up"
+            
+        it "Error log" $ do
+            parseMessage "E 20 2 Too many pickles" `shouldBe` LogMessage (Error 20) 2 "Too many pickles"
 
-    it "Unknown log" $ do
-        parseMessage "This is not in the right format" `shouldBe` Unknown "This is not in the right format"
-        
+        it "Unknown log" $ do
+            parseMessage "This is not in the right format" `shouldBe` Unknown "This is not in the right format"
+    
+    describe "insert" $ do
+        it "returns unchanged tree when given unknown log message" $ do
+            insert (Unknown "") smallTree `shouldBe` smallTree
+        it "Insert logMessage to smallTree" $ do
+            let a = insert (LogMessage (Error 100) 2 "2222") smallTree
+            let b = insert (LogMessage Info 4 "4444") a
+            b `shouldBe` newTree
+
+    describe "build" $ do
+        it "returns Leaf when []" $ do
+            build [] `shouldBe` Leaf
+        it "return newTree" $ do
+            build logMessages `shouldBe` newTree
+    
+    describe "inOrder" $ do
+        it "return sorted log message when newTree input" $ do
+            inOrder newTree `shouldBe` sortedLogMessages
+    
+    describe "whatWentWrong" $ do
+        it "return revelent error message string" $ do
+            whatWentWrong postmortemMessages `shouldBe` postmortemResult
