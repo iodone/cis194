4c7566afb7ba23b8bd9e8899b6b16461d4d05966
Your Name <you@example.com>
2019-01-15 15:19:11 +0800

    Complete homework03 Ex.3

--- src/Homework03/Golf.hs
+++ src/Homework03/Golf.hs
@@ -13,3 +13,26 @@ everyNth n xs =
         (y:ys) -> y : (everyNth n ys)
     where rest = drop (n-1) xs
 
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima [_] = []
+localMaxima [_,_] = []
+localMaxima (x:y:z:zs)
+    | (y > x && y > z) = y : (localMaxima rest)
+    | otherwise = localMaxima (rest)
+    where rest = y:z:zs
+
+
+
+histogram :: [Integer] -> String
+histogram xs = 
+    let 
+        frequences = trans xs
+        height = foldr1 max frequences 
+        visualized = [['0'..'9']] ++ [(replicate 10 '=')] ++ map (\index -> [if e >= index  then '*' else ' ' | e <- frequences]) [1..height]
+    in
+        unlines (reverse visualized)
+        
+trans :: [Integer] -> [Integer]
+trans xs = map (\i -> (toInteger (length [x | x <- xs, x == i]))) [0..9]
--- test/Homework03/GolfSpec.hs
+++ test/Homework03/GolfSpec.hs
@@ -2,7 +2,24 @@ module Homework03.GolfSpec where
 
 import Homework03.Golf
 import Test.Hspec
-    
+
+histogram1 :: String
+histogram1 = unlines
+  [ " *        "
+  , " *        "
+  , " *   *    "
+  , "=========="
+  , "0123456789" ]
+
+histogram2 :: String
+histogram2 = unlines
+  [ "    *     "
+  , "    *     "
+  , "    * *   "
+  , " ******  *"
+  , "=========="
+  , "0123456789" ]
+
 
 spec :: Spec
 spec = describe "homework03.Golf" $ do
@@ -15,3 +32,20 @@ spec = describe "homework03.Golf" $ do
             skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
             skips [1] `shouldBe` ([[1]] :: [[Integer]])
             skips [True,False] `shouldBe` [[True,False], [False]]
+
+    describe "localMaxima" $ do
+        it "for boundry case" $ do
+            localMaxima [] `shouldBe` []
+            localMaxima [3] `shouldBe` []
+            localMaxima [3,2] `shouldBe` []
+        it "for more normal case"  $ do
+            localMaxima [2,9,5,6,1] `shouldBe` [9,6]
+            localMaxima [2,3,4,1,5] `shouldBe` [4]
+            localMaxima [1,2,3,4,5] `shouldBe` []
+    
+    describe "histogram" $ do
+        it "works for one input" $ do
+            histogram [1,1,1,5] `shouldBe` histogram1
+    
+        it "works for another input" $ do
+            histogram [1,4,5,4,6,6,3,4,2,4,9] `shouldBe` histogram2
